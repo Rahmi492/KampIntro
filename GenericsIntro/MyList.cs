d24775764152cd4db3fa99b2976c1191242d835d
rahmi koç <emutrah@outlook.com>
2021-04-20 00:39:08 +0300

    c# 4. ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //Constructor      
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item;
+
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product { ProductName = "ütü", Id = 1, CatagoryId = 1 , UnitInStock=5, UnitPrice=300 };
+            Product product2 = new Product {CatagoryId=2, UnitPrice=200, UnitInStock=3, Id=2, ProductName="çarşaf" };
+            PruductManager pruductManager = new PruductManager();
+            pruductManager.Add(product1);
+        }
+    }
+}
